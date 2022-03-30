3430171d81ffe9a67432e700089440c94b0e0521
econuri <btjung7@gmail.com>
2022-03-30 22:30:44 +0900

    3rd : useParams error

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,11 +1,14 @@
 import dummy from "../db/data.json";
+import React from "react";
+import { useParams } from "react-router";
 
 export default function Day() {
-    const day = 1;
-    const wordList = dummy.words.filter(word => word.day === day);
-    console.log(wordList);
 
     return <>
+        const {day} = useParams();        
+        const wordList = dummy.words.filter(word => word.day === Number(day)) ;
+
+        <h2>Day {day}</h2>
         <table>
             <tbody>
                 {wordList.map(word => (
