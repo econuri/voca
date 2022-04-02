b16462cc861be3ac73d7bfd9b2697d9af5825be8
econuri <btjung7@gmail.com>
2022-04-02 11:31:15 +0900

    end of lecture

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,24 +1,27 @@
-import React, { useState, useEffect } from "react";
+//import React, { useState, useEffect } from "react";
 import { useParams } from "react-router";
 import Word from "./Word";
+import useFetch from "../hooks/useFetch";
 
 export default function Day() {
     const {day} = useParams();        
-    const [words, setWords] = useState([]);
+    // const [words, setWords] = useState([]);
 
-    useEffect( () => {
-        fetch(`http://localhost:3001/words?day=${day}`)
-        .then(res => {
-            return res.json();
-        })
-        .then(data => {
-            setWords(data);
-        });
-    }, [day]);
+    // useEffect( () => {
+    //     fetch(`http://localhost:3001/words?day=${day}`)
+    //     .then(res => {
+    //         return res.json();
+    //     })
+    //     .then(data => {
+    //         setWords(data);
+    //     });
+    // }, [day]);
 
+    const words = useFetch(`http://localhost:3001/words?day=${day}`);
     return (
     <>
         <h2>Day {day}</h2>
+        {words.length === 0 && <span>Loading...</span>}
         <table>
             <tbody>
                 {words.map(word => (
--- src/component/DayList.js
+++ src/component/DayList.js
@@ -1,19 +1,25 @@
-import { useEffect, useState } from "react";
+//import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
 
 export default function DayList() {
-    const [days, setDays] = useState([]);
+    const days = useFetch("http://localhost:3001/days");
+    // const [days, setDays] = useState([]);
 
-    useEffect(() => {
-        fetch('http://localhost:3001/days')
-        .then(res => {
-            return res.json();
-        })
-        .then(data => {
-            setDays(data);
-        });
-    }, []);
+    // useEffect(() => {
+    //     fetch('http://localhost:3001/days')
+    //     .then(res => {
+    //         return res.json();
+    //     })
+    //     .then(data => {
+    //         setDays(data);
+    //     });
+    // }, []);
 
+    if(days.length === 0) {
+        return <span>Loading...</span>
+    }
+    
     return (
         <ul className="list_day">
             {days.map(day => (
